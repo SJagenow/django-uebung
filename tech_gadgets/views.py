6b7566021628655bb08cf02261a4099263e6c50f
SJagenow <sjagenow@web.de>
2024-09-16 15:03:24 +0200

    view

--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def start_page_view(request):
+    return HttpResponse("hey das hat doch gut funktioniert!")
