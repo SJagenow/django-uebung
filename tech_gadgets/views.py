30eb30d31cb1c9755f66a22b1f881116b64597b6
SJagenow <sjagenow@web.de>
2024-09-20 10:17:48 +0200

    django_gadget_as_view

--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -4,33 +4,53 @@ import json
 from django.utils.text import slugify
 from django.urls import reverse
 from .dummy_data import gadgets
+from django.views import View
 
 def start_page_view(request):
     return HttpResponse("hey das hat doch gut funktioniert!")
 
-def single_gadget_view(request,gadget_id):
+def single_gadget_int_view(request,gadget_id):
     if len(gadgets) > gadget_id:
       new_slug = slugify(gadgets[gadget_id]["name"]) 
       new_url = reverse("gadget_slug_url", args=[new_slug])
       return redirect(new_url)
     return HttpResponseNotFound("not found by me")
 
-def single_gadget_slug_view(request,gadget_slug):
-    gadget_match = {"result": "nothing"}
 
-    for gadget in gadgets:
-        if slugify(gadget["name"]) == gadget_slug:
-           gadget_match = gadget  
+class GadgetView(View):
+     def get(self, request, gadget_slug):
+         gadet_match = None
+         for gadget in gadgets:
+           if slugify(gadget["name"]) == gadget_slug:
+              gadget_match = gadget
 
-    if gadget_match: 
-       return JsonResponse(gadget_match)
-    raise Http404()
+           if gadget_match:
+               return JsonResponse(gadget_match)
+           raise Http404() 
+         def post(self, request, *args, **kwargs):
+                 try:
+                  data = json.loads(request.body)
+                  print(f"recived data: {data}")
+                  return JsonResponse({"response": "Das war was"})
+                 except:
+                  return JsonResponse({"response": "Das war wohl nix"})
 
-def single_gadget_post_view(request):
+
+
+
+def single_gadget_view(request,gadget_slug=""):
+   
+    if request.method == "GET":
+       gadet_match = None
+       for gadget in gadgets:
+           if slugify(gadget["name"]) == gadget_slug:
+              gadget_match = gadget
+
+           if gadget_match:
+               return JsonResponse(gadget_match)
+               raise Http404()   
+
     if request.method == "POST":
-        try:
-           data = json.loads(request.body)
-           print(f"recived data: {data}")
-           return JsonResponse({"response": "Das war was"})
-        except:
-            return JsonResponse({"response": "Das war wohl nix"})
+       
+    
+    
